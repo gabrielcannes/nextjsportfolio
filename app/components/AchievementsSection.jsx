7034281344f5bec85e5d99e3308d818609f16c70
gabrielcannes <gabrielcannes.gc@gmail.com>
2025-05-20 13:32:52 -0300

    Ajuste no espacamento das conquistas em tela de celular

--- app/components/AchievementsSection.jsx
+++ app/components/AchievementsSection.jsx
@@ -31,7 +31,7 @@ const achievementsList = [
 export const AchievementsSection = () => {
   return (
     <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-      <div className="border-[#33353F] border rounded-md py-8 px-16 flex flex-row items-center justify-between">
+      <div className="border-[#33353F] border rounded-md py-8 md:px-16 flex flex-row items-center justify-between">
         {achievementsList.map((a, index) => {
           return (
             <div
